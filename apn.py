7ea5f00c7922d121dd1214a347600ccaec85896c
Janet Zhang <janetzhang321@gmail.com>
2018-11-20 20:04:51 -0500

    done q 1

--- apn.py
+++ apn.py
@@ -1,15 +1,27 @@
-def euclid(x, y):
+def euclid(x,y):
+    print("finding the gcd(",x,",",y,")...")
     if (x < 0):
+        print(x,"is negative")
         x = -1*x
+        print("we multiply",-1*x,"by -1 to get",x)
     if (y < 0):
+        print(y,"is negative")
         y = -1*y
+        print("we multiply",-1*y,"by -1 to get",y)
+
+    print(x,"mod",y, "=", x%y)
+        
     if (x % y == 0):
-        return y
+        print("\nthe gcd is",y)
+        
     else:
-        return euclid(y,x%y)
+        euclid(y,x%y)
 
-
+def test_euclid():
+    x = input("enter first number: ")
+    y = input("enter second number: ")
+    euclid(int(x),int(y))
+        
 if (__name__=="__main__"):
-    x = 50
-    y = -10
-    print(euclid(x,y))
+    #test_euclid()
+    
